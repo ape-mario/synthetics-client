e29cde7ff8b41d414a0366983ce215a336381197
tony-cap <tony-cap@users.noreply.github.com>
2020-09-24 22:34:21 +0200

    improve styling

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -7,29 +7,30 @@
 		display: flex;
 		align-items: center;
 		justify-content: space-between;
-		height: 55px;
 		border-bottom: 3px solid var(--border-color);
 		position: relative;
 		z-index: 12;
 	}
+	header > div {
+		margin-bottom: 10px;
+	}
 	.logo-container {
 		display: flex;
+		flex-wrap: wrap;
 		align-items: center;
-		justify-content: space-between;
+		margin-right: 5px;
 	}
 	.logo img {
-		height: 28px;
+		height: 25px;
 		margin-top: 1px;
-		margin-right: 15px;
+		margin-right: 10px;
 	}
 </style>
 
 <header>
 	<div class='logo-container'>
-		<a class='logo' href='/'><img alt='Cap Logo' src='/assets/logo-pure.svg' /></a>
+		<span class='logo'><img alt='Cap Logo' src='/assets/logo-pure.svg' /></span>
 		<span><strong>Synthetics</strong></span>
 	</div>
-	<div class='wallet'>
-		<Wallet/>
-	</div>
+	<Wallet/>
 </header>
