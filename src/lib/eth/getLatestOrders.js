828fc92163d35391b94e41ca28c643cc9b1c7b1a
tony-cap <tony-cap@users.noreply.github.com>
2020-10-02 19:44:45 +0100

    fetch data from the last ~3 months

--- src/lib/eth/getLatestOrders.js
+++ src/lib/eth/getLatestOrders.js
@@ -1,13 +1,17 @@
 import { contract } from '../constants'
 import getFilterLogs from './getFilterLogs'
 import getFilterChanges from './getFilterChanges'
+import getBlockByNumber from './getBlockByNumber'
 import { extractLogData, formatUser, enrichOrderSubmittedEventData, EVENT_TYPES, ORDER_SUBMITTED_EVENT_TYPES, PROCESSING_EVENT_TYPES } from '../eventHelpers'
 
 const LIMIT = 10;
+const BLOCK_COUNT = 777600n;
 
 export async function getLatestOrders(params) {
 	const { user } = params;
-	const logs = await getFilterLogs({ fromBlock: 'earliest', addresses: [ contract('CAP_ASSETS') ], topics: [EVENT_TYPES, formatUser(user)] });
+	const currentBlock = await getBlockByNumber();
+	const fromBlock = BigInt(currentBlock.number) > BLOCK_COUNT ? `0x${(BigInt(currentBlock.number) - BLOCK_COUNT).toString(16)}` : 'earliest';
+	const logs = await getFilterLogs({ fromBlock, addresses: [ contract('CAP_ASSETS') ], topics: [EVENT_TYPES, formatUser(user)] });
 	const orderSubmittedEvents = logs.filter(e => ORDER_SUBMITTED_EVENT_TYPES.includes(e.topics[0])).slice(-1 * LIMIT).map(extractLogData);
 	const startIndex = logs.findIndex(e => e.transactionHash == orderSubmittedEvents[0].txhash);
 	const processingEvents = Object.assign({}, ...logs.slice(startIndex).filter(e => !ORDER_SUBMITTED_EVENT_TYPES.includes(e.topics[0])).map(extractLogData));
